d91044c51232338fb08315b3eee8977e18e388f2
Ilia Merkulov <77540282+IlyaM70@users.noreply.github.com>
2025-02-14 17:22:14 -0800

    Auth Slice created User Interface created

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -1,5 +1,11 @@
 import menuItemInterface from "./menuItemInterface";
 import cartItemInterface from "./cartItemInterface";
 import shoppingCartModel from "./shoppingCartModel";
+import userInterface from "./userInterface";
 
-export type { menuItemInterface, cartItemInterface, shoppingCartModel };
+export type {
+  menuItemInterface,
+  cartItemInterface,
+  shoppingCartModel,
+  userInterface,
+};
