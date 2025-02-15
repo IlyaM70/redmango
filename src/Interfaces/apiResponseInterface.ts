4b9c250c2683c55c5573a6cbfbcfd462f391ba58
Ilia Merkulov <77540282+IlyaM70@users.noreply.github.com>
2025-02-14 17:44:04 -0800

    Added response API interface

---
+++ src/Interfaces/apiResponseInterface.ts
@@ -0,0 +1,12 @@
+export default interface apiResponseInterface {
+  data?: {
+    statusCode?: number;
+    isSuccess?: boolean;
+    errorMessages?: Array<string>;
+    result?: {
+      // this will not give you suggestions
+      [key: string]: string;
+    };
+  };
+  error?: any;
+}
--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -2,10 +2,12 @@ import menuItemInterface from "./menuItemInterface";
 import cartItemInterface from "./cartItemInterface";
 import shoppingCartModel from "./shoppingCartModel";
 import userInterface from "./userInterface";
+import apiResponseInterface from "./apiResponseInterface";
 
 export type {
   menuItemInterface,
   cartItemInterface,
   shoppingCartModel,
   userInterface,
+  apiResponseInterface,
 };
