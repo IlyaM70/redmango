f5ea52914d75b0b7c421d123d4fdfaa0af4ea5d9
Ilia Merkulov <77540282+IlyaM70@users.noreply.github.com>
2025-02-28 14:09:11 -0800

    Initialized Order API

--- src/Storage/Redux/store.ts
+++ src/Storage/Redux/store.ts
@@ -1,6 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { menuItemReducer } from "./menuItemSlice";
-import { menuItemApi, shoppingCartApi, authApi, paymentApi } from "../../Apis";
+import {
+  menuItemApi,
+  shoppingCartApi,
+  authApi,
+  paymentApi,
+  orderApi,
+} from "../../Apis";
 import { shoppingCartReducer } from "./shoppingCartSlice";
 import { userAuthReducer } from "./userAuthSlice";
 const store = configureStore({
@@ -12,13 +18,15 @@ const store = configureStore({
     [shoppingCartApi.reducerPath]: shoppingCartApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
     [paymentApi.reducerPath]: paymentApi.reducer,
+    [orderApi.reducerPath]: orderApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(menuItemApi.middleware)
       .concat(shoppingCartApi.middleware)
       .concat(authApi.middleware)
-      .concat(paymentApi.middleware),
+      .concat(paymentApi.middleware)
+      .concat(orderApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
