80dc06b50ffc21ea564ff00b1bda639a0082d49a
Ilia Merkulov <77540282+IlyaM70@users.noreply.github.com>
2025-03-15 13:27:06 -0700

    Added search functionality at Home page

--- src/Components/Page/Home/MenuItemList.tsx/MenuItemList.tsx
+++ src/Components/Page/Home/MenuItemList.tsx/MenuItemList.tsx
@@ -1,20 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { menuItemInterface } from "../../../../Interfaces";
 import MenuItemCard from "./MenuItemCard";
 import { useGetMenuItemsQuery } from "../../../../Apis/menuItemApi";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setMenuItem } from "../../../../Storage/Redux/menuItemSlice";
 import { MainLoader } from "../../Common";
+import { RootState } from "../../../../Storage/Redux/store";
 
 function MenuItemList() {
+  const [menuItems, setMenuItems] = useState<menuItemInterface[]>([]);
   const { data, isLoading } = useGetMenuItemsQuery(null);
   const dispath = useDispatch();
+
+  const searchValue = useSelector(
+    (state: RootState) => state.menuItemStore.search
+  );
+
+  useEffect(() => {
+    if (data && data.result) {
+      const tempMenuItems = handleFilters(searchValue);
+      setMenuItems(tempMenuItems);
+    }
+  }, [searchValue]);
 
   useEffect(() => {
     if (!isLoading) {
       dispath(setMenuItem(data.result));
+      setMenuItems(data.result);
     }
   }, [isLoading]);
+
+  const handleFilters = (search: string) => {
+    let tempMenuItems = [...data.result];
+
+    if (search) {
+      tempMenuItems = tempMenuItems.filter(
+        (item: menuItemInterface) =>
+          item.name.toLowerCase().includes(search.toLowerCase()) ||
+          item.description.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    return tempMenuItems;
+  };
 
   if (isLoading) {
     return <MainLoader />;
@@ -22,8 +50,8 @@ function MenuItemList() {
 
   return (
     <div className="container row">
-      {data.result.length > 0 &&
-        data.result.map((menuItem: menuItemInterface, index: number) => (
+      {menuItems.length > 0 &&
+        menuItems.map((menuItem: menuItemInterface, index: number) => (
           <MenuItemCard menuItem={menuItem} key={index} />
         ))}
     </div>
