6eb7734f0446e85ece737a65d810d9e393247696
Ilia Merkulov <77540282+IlyaM70@users.noreply.github.com>
2025-03-18 16:37:29 -0700

    Added Bootstrap Icons stylesheet; removed Bootstrap Icons dependency from package.json and package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import "./index.css";
 import App from "./Container/App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Storage";
